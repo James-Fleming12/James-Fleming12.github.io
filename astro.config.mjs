ee02589094fc08b4a5e45a20015e39722a07c31f
James Fleming <jamesfleming.sd@gmail.com>
2026-07-01 21:23:01 -0700

    another bugfix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import { unified } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://james-fleming12.github.io',
     integrations: [mdx(), sitemap()],
     markdown: {
-        remarkPlugins: [remarkMath],
-        rehypePlugins: [rehypeKatex]
+        processor: unified({
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex]
+        })
     }
 });
